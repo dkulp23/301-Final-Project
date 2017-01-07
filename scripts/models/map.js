efbff65ed55db2566a111c432a76f3517f6911ca
Maithrika Sara <maithrika@Consciously-Uncoupling.local>
2017-01-07 15:02:50 -0800

    geolocation now shows marker and circle around showing accuracy

--- scripts/models/map.js
+++ scripts/models/map.js
@@ -8,22 +8,22 @@ function initMap(pos) {
     styles: [{'featureType':'administrative','elementType':'labels.text.fill','stylers':[{'color':'#444444'}]},{'featureType':'landscape','elementType':'all','stylers':[{'color':'#f2f2f2'}]},{'featureType':'landscape.man_made','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#eae9ed'}]},{'featureType':'landscape.natural','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#d2e0b7'}]},{'featureType':'landscape.natural.landcover','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#d2e0b7'}]},{'featureType':'landscape.natural.terrain','elementType':'geometry','stylers':[{'visibility':'on'},{'color':'#d2e0b7'}]},{'featureType':'poi','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'poi.park','elementType':'geometry.fill','stylers':[{'visibility':'on'}]},{'featureType':'road','elementType':'all','stylers':[{'saturation':-100},{'lightness':45}]},{'featureType':'road','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#ffffff'}]},{'featureType':'road','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'labels.text.stroke','stylers':[{'visibility':'off'}]},{'featureType':'road.highway','elementType':'all','stylers':[{'visibility':'simplified'}]},{'featureType':'road.arterial','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'transit','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'water','elementType':'all','stylers':[{'color':'#b3dced'},{'visibility':'on'}]}]
   });
 
-  var getAddress = function(pos){
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-      'location': pos
-    },
-    function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        console.log(results[0].formatted_address);
-        return results[0].formatted_address;
-      }
-      else {
-        return 'Unable to retrieve your address';
-      }
-    });
-  };
-  console.log(getAddress());
+  // var getAddress = function(pos){
+  //   var geocoder = new google.maps.Geocoder();
+  //   geocoder.geocode({
+  //     'location': pos
+  //   },
+  //   function(results, status) {
+  //     if (status == google.maps.GeocoderStatus.OK) {
+  //       console.log(results[0].formatted_address);
+  //       return results[0].formatted_address;
+  //     }
+  //     else {
+  //       return 'Unable to retrieve your address';
+  //     }
+  //   });
+  // };
+  // console.log(getAddress());
 
   var marker = new google.maps.Marker({
     position: pos,
@@ -31,7 +31,7 @@ function initMap(pos) {
   });
   var infoWindow = new google.maps.InfoWindow(
     {
-      content: getAddress()
+      content: 'You are here'
     }
   );
   marker.addListener('click', function() {
@@ -84,7 +84,7 @@ function initMap(pos) {
     //   enableHighAccuracy: true
     // };
     navigator.geolocation.getCurrentPosition(function(position) {
-      window.pos = {
+      var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
